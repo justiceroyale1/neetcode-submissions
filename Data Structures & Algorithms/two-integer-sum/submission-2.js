59950c7a8c075a7bbb8b9e5e27e3a65a90fe24c4
Abutu Justice Royale <justiceroyale1@gmail.com>
2026-06-13 16:46:58 +0100

    Add: two-integer-sum - submission-2

---
+++ Data Structures & Algorithms/two-integer-sum/submission-2.js
@@ -0,0 +1,39 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let diffs = new Map();
+        let a
+        let b;
+
+        // diffs = nums.map((item, index) => {
+        //     let diff = target - item;
+
+        //     return {
+        //         index,
+        //         diff: target - item,
+        //         exists: nums.filter((item, index2) => index2 != index).includes(diff)
+        //     }
+        // })
+
+        for(let i = 0; i < nums.length; i++){
+            let difference = target - nums[i];
+
+            if(diffs.has(difference)){
+                a = i;
+                b = diffs.get(difference);
+            }else{
+                diffs.set(nums[i], i);
+            }
+        }
+
+        if(a != b && a !== undefined && b !== undefined){
+            return [a, b];
+        }
+
+        // return diffs.filter(item => item.exists).map(item => item.index);
+    }
+}
